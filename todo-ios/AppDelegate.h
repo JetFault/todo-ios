f4b159485b833df57f4a32f6c726a2ed96128d76
Jerry Reptak <rjerry@yahoo-inc.com>
2014-01-20 16:52:54 -0800

    Initial Commit

---
+++ todo-ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  todo-ios
+//
+//  Created by Jerry Reptak on 1/20/14.
+//  Copyright (c) 2014 Jerry Reptak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ todo-iosTests/todo_iosTests.m
@@ -0,0 +1,34 @@
+//
+//  todo_iosTests.m
+//  todo-iosTests
+//
+//  Created by Jerry Reptak on 1/20/14.
+//  Copyright (c) 2014 Jerry Reptak. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface todo_iosTests : XCTestCase
+
+@end
+
+@implementation todo_iosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
